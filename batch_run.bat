91163d9d1d4388306b0ff855b8bc0acb830aa219
Cool <ryan.cool@yale.edu>
2024-04-30 15:27:09 -0400

    added batch file to run py script

---
+++ batch_run.bat
@@ -0,0 +1,7 @@
+@echo off
+"C:\redcap_backups\src\env\Scripts\python.exe" "C:\redcap_backups\src\main.py" "cosgrove" "records"
+"C:\redcap_backups\src\env\Scripts\python.exe" "C:\redcap_backups\src\main.py" "cosgrove" "project"
+"C:\redcap_backups\src\env\Scripts\python.exe" "C:\redcap_backups\src\main.py" "davis" "records"
+"C:\redcap_backups\src\env\Scripts\python.exe" "C:\redcap_backups\src\main.py" "davis" "project"
+"C:\redcap_backups\src\env\Scripts\python.exe" "C:\redcap_backups\src\main.py" "esterlis" "records"
+"C:\redcap_backups\src\env\Scripts\python.exe" "C:\redcap_backups\src\main.py" "esterlis" "project"
