54fc876dddafc2838809ac95982d51ab7e5174a8
Ryan Cool <ryantcool@pm.me>
2025-01-27 15:38:14 -0500

    Changes
    - Updated formatting via ruff linter
    - Removed timedelta import
    - Re-worked date comparision in date check function
    - Added testing output_dir for when working on linux vs pc

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ This script is used to query data from multiple redcap projects via api tokens
 and save the data as a form of backup.
 """
 
-import sys
-import os
-import requests
 import json
-from datetime import datetime, timedelta
+import os
+import sys
+from datetime import datetime
+
+import requests
 from bs4 import BeautifulSoup
 
 __author__ = "Ryan Cool"
@@ -27,31 +28,31 @@ __status__ = "Dev"
 # Exports all records
 
 records_data = {
-    'token': '',
-    'content': 'record',
-    'action': 'export',
-    'format': 'csv',
-    'type': 'flat',
-    'csvDelimiter': ',',
-    'rawOrLabel': 'raw',
-    'rawOrLabelHeaders': 'raw',
-    'exportCheckboxLabel': 'false',
-    'exportSurveyFields': 'false',
-    'exportDataAccessGroups': 'false',
-    'returnFormat': 'json'
+    "token": "",
+    "content": "record",
+    "action": "export",
+    "format": "csv",
+    "type": "flat",
+    "csvDelimiter": ",",
+    "rawOrLabel": "raw",
+    "rawOrLabelHeaders": "raw",
+    "exportCheckboxLabel": "false",
+    "exportSurveyFields": "false",
+    "exportDataAccessGroups": "false",
+    "returnFormat": "json",
 }
 
 # Exports entire project
 
 project_data = {
-    'token': '',
-    'content': 'project_xml',
-    'format': 'xml',
-    'returnMetadataOnly': 'false',
-    'exportFiles': 'true',
-    'exportSurveyFields': 'true',
-    'exportDataAccessGroups': 'true',
-    'returnFormat': 'xml'
+    "token": "",
+    "content": "project_xml",
+    "format": "xml",
+    "returnMetadataOnly": "false",
+    "exportFiles": "true",
+    "exportSurveyFields": "true",
+    "exportDataAccessGroups": "true",
+    "returnFormat": "xml",
 }
 
 #############################
@@ -61,7 +62,7 @@ project_data = {
 
 def load_config():
     script_dir = os.path.dirname(__file__)
-    path_to_config = os.path.join(script_dir, 'config.json')
+    path_to_config = os.path.join(script_dir, "config.json")
     with open(path_to_config) as config_file:
         config = json.load(config_file)
     return path_to_config, config
@@ -70,26 +71,25 @@ def load_config():
 path_to_config, config = load_config()
 
 
-def set_token(pi_name):
-    api_key = config['tokens'][0][pi_name]
-    records_data['token'] = api_key
-    project_data['token'] = api_key
+def set_token(pi_name: str):
+    api_key = config["tokens"][0][pi_name]
+    records_data["token"] = api_key
+    project_data["token"] = api_key
 
 
 def date_check():
-    date = config['date']
+    date = config["date"]
     if not date:
-        date = datetime.today().strftime('%Y-%m-%d')
-        config['date'] = date
+        date = datetime.today().strftime("%Y-%m-%d")
+        config["date"] = date
     else:
         pass
-    date_object = datetime.strptime(date, '%Y-%m-%d')
-    seven_days_ago = date_object + timedelta(days=7)
-    if seven_days_ago.date() == datetime.today().date():
-        print("The date is 7 days ago")
-        date = datetime.today().strftime('%Y-%m-%d')
-        config['date'] = date
-        with open(path_to_config, 'w') as config_file:
+    date_object = datetime.strptime(date, "%Y-%m-%d")
+    if (datetime.today() - date_object).days >= 7:
+        print("The date is/is over 7 days ago")
+        date = datetime.today().strftime("%Y-%m-%d")
+        config["date"] = date
+        with open(path_to_config, "w") as config_file:
             json.dump(config, config_file, indent=4, sort_keys=True)
         return True
     else:
@@ -97,24 +97,34 @@ def date_check():
         return False
 
 
-def file_org(pi, export_type, file_ext, records, output_dir):
-    export_date = datetime.today().strftime('%Y-%m-%d')
+def file_org(
+    pi: str, export_type: str, file_ext: str, records: str, output_dir: str
+):
+    export_date = datetime.today().strftime("%Y-%m-%d")
     title = f"all_{pi}_studies"
     file_name = f"{export_date}_{title}_{export_type}.{file_ext}"
-
+    output_dir = os.path.join(output_dir, "")
     if file_ext == "xml":
         soup = BeautifulSoup(records, "xml")
-        with open(output_dir + file_name, "w", encoding="utf-8") as file:
+        with open(
+            os.path.join(output_dir, file_name), "w", encoding="utf-8"
+        ) as file:
             file.write(str(soup.prettify()))
     elif file_ext == "csv":
-        with open(output_dir + file_name, "w") as file:
+        with open(os.path.join(output_dir, file_name), "w") as file:
             file.write(records)
 
 
 def main():
-    output_dir = "C:\\redcap_backups\\daily\\"
+    # File Path for main PC
+    # output_dir = "C:\\redcap_backups\\daily\\"
+    # File Path for testing
+    output_dir = os.path.join(os.path.dirname(__file__), "redcap_output/daily")
     pi, export_type = sys.argv[1:3]
-    if pi not in ["cosgrove", "davis", "esterlis"] or export_type not in ["records", "project"]:
+    if pi not in ["cosgrove", "davis", "esterlis"] or export_type not in [
+        "records",
+        "project",
+    ]:
         print("Invalid arguments provided.")
         exit(1)
 
@@ -131,8 +141,8 @@ def main():
         exit(1)
 
     try:
-        r = requests.post('https://poa-redcap.med.yale.edu/api/', data=api_call)
-        print('HTTP Status: ' + str(r.status_code))
+        r = requests.post("https://poa-redcap.med.yale.edu/api/", data=api_call)
+        print("HTTP Status: " + str(r.status_code))
         records = r.text
     except Exception as e:
         print(f"Error occurred: {e}")
